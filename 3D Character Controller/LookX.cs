2c0fa2a957d31e9e2e77c22e455446a28eb06113
Johnathan Banks <77426339+banks47j@users.noreply.github.com>
2021-10-03 12:58:20 -0500

    Add files via upload

---
+++ 3D Character Controller/LookX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1f;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        float _mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 newrotation = transform.localEulerAngles;
+        newrotation.y += _mouseX * _sensitivity;
+        transform.localEulerAngles = newrotation;
+    }
+}
